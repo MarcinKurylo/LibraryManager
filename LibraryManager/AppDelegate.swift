1b7076041a282647095b838e96279cae1fcd96ab
MarcinKurylo <marcin.kurylo.lo@gmail.com>
2021-03-14 18:09:41 +0100

    Initial Commit

---
+++ LibraryManager/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LibraryManager
+//
+//  Created by Marcin Kuryło on 14/03/2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LibraryManager/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  LibraryManager
+//
+//  Created by Marcin Kuryło on 14/03/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
